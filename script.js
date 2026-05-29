ef6b7d548e33667fda506a6aac4968ab90ca95ec
getkrowd-del <getkrowd@gmail.com>
2026-05-29 07:37:59 -0400

    Update landing page: Fox Small Engine Sales & Service - 2026-05-29T11:37:56.765Z

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
-      anchor.addEventListener('click', function(e) {
-        var target = document.querySelector(this.getAttribute('href'));
-        if (target) { e.preventDefault(); target.scrollIntoView({ behavior: 'smooth', block: 'start' }); }
+document.querySelectorAll('a[href^="#"]').forEach(function(a) {
+      a.addEventListener('click', function(e) {
+        var t = document.querySelector(this.getAttribute('href'));
+        if (t) { e.preventDefault(); t.scrollIntoView({ behavior: 'smooth', block: 'start' }); }
       });
     });
